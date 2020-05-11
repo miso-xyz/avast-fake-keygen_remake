99a2faf5c0311efdf4f3d359e4d5a5633441eacd
miso-xyz <61099345+miso-xyz@users.noreply.github.com>
2020-05-11 01:52:35 +0100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Dim blnButtonDown, blnButtonDown_
+    Private Sub Button1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Button1.Paint
+        If blnButtonDown = False Then
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Raised)
+        Else
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Sunken)
+        End If
+    End Sub
+    Private Sub Button1_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseDown
+        blnButtonDown = True
+        sender.Invalidate()
+    End Sub
+
+    Private Sub Button1_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseUp
+        blnButtonDown = False
+        sender.Invalidate()
+    End Sub
+    Private Sub RichTextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RichTextBox1.TextChanged
+        RichTextBox1.SelectAll()
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        RichTextBox1.SelectAll()
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        End
+    End Sub
+    Private Sub Button2_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Button2.Paint
+        If blnButtonDown_ = False Then
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Raised)
+        Else
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Sunken)
+        End If
+    End Sub
+    Private Sub Button2_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button2.MouseDown
+        blnButtonDown_ = True
+        sender.Invalidate()
+    End Sub
+
+    Private Sub Button2_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button2.MouseUp
+        blnButtonDown_ = False
+        sender.Invalidate()
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim keys As New List(Of String)({"S8514758H7685M1005-7AN6PNVH", "S6808081H7360M0605-RP6J7YCK", "W3125747R8800X1106-W6D2XK5R", "S5619270H9879M1105-LXVAE13W", "W8634658R8800V1106-ELTBMUSY", "S5409938M5797M0805-NH8ZN49Z", "S7717331M4556M1105-MCC0ZX8D"})
+        Dim rng_ As New Random
+        RichTextBox1.Text = keys(rng_.Next(0, 6))
+    End Sub
+End Class
